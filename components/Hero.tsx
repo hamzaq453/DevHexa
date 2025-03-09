e7f23c8d9942f50df37336feb14912d373648187
Hamza Qureshi <bc220407660mhq@vu.edu.pk>
2025-03-09 08:06:46 +0500

    Navbar and Hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+import { HeroGeometric } from "@/components/ui/shape-landing-hero"
+import { SplashCursor } from "./ui/splash-cursor"
+
+export default function Hero() {
+  return (
+    <div>
+    <SplashCursor />
+    <HeroGeometric badge="Kokonut UI"
+      title1 = "Elevate Your"
+      title2 = "Digital Vision" />
+   
+    </div>
+  )
+}
